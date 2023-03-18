d3e06f8312e946542267f2933a6e31bb6f51c3db
Lukasz Nogaj <lukasznogaj@Lukaszs-Mac-mini.local>
2023-03-18 15:00:59 +0000

    start2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@ import './Header.css';
 
 const Header = () => {
   return (
-    <div>
+    <div className='header'>
       <h1 className='top__head'>BMI healthy weight calculator</h1>
-      <p>
+      <p className='top__paragraph'>
         Use this calculator to check your body mass index (BMI) and find out if
         your're healthy weight. Or you can use it to check your child's BMI
       </p>
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../Layout.css';
+import './Layout.css';
 import Header from '../Header';
 
 /* Main conatiner with the childrens */

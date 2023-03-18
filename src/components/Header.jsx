720d99a0732d88274593ccbbae273fd6489f719d
Lukasz Nogaj <lukasznogaj@Lukaszs-Mac-mini.local>
2023-03-18 14:32:19 +0000

    start

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className='top__head'>BMI healthy weight calculator</h1>
+      <p>
+        Use this calculator to check your body mass index (BMI) and find out if
+        your're healthy weight. Or you can use it to check your child's BMI
+      </p>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../Layout.css';
+import Header from '../Header';
+
+/* Main conatiner with the childrens */
+
+const Layout = () => {
+  return (
+    <div className='container'>
+      <Header />
+    </div>
+  );
+};
+
+export default Layout;

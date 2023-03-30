eddede55e59db9ad8fc1771ce6830f9fd755d073
Lukasz Nogaj <lukasznogaj@Lukaszs-Mac-mini.local>
2023-03-30 14:03:15 +0100

    handler

--- src/components/Age/TabsAge.jsx
+++ src/components/Age/TabsAge.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react';
 import styles from './TabsAge.module.css';
 const TabsAge = () => {
-  const [isConfirmed, setIsConfirmed] = useState(false);
+  const [isConfirmed, setIsConfirmed] = useState('Adult');
 
-  const clickHandler = (event) => {
+  const clickHandler = (age) => {
     setIsConfirmed(!isConfirmed);
     console.log('Clicked');
   };
@@ -12,18 +12,21 @@ const TabsAge = () => {
       {' '}
       <div
         onClick={clickHandler}
-        className={`${styles.tabsAgeBtn} ${
-          isConfirmed ? styles.tabsAgeConfirmed : styles.tabsAgeConfirmed
-        } `}
+        className={styles.tabsAgeBtn}
+        style={{
+          backgroundColor: isConfirmed === 'Adult' ? '#778692' : 'blue',
+        }}
       >
         Adult
       </div>
       <div
         onClick={clickHandler}
-        className={`${styles.tabsAgeBtn} ${
-          isConfirmed ? styles.tabsAgeConfirmed : styles.tabdAgeBtn
-        }`}
+        className={styles.tabsAgeBtn}
+        style={{
+          backgroundColor: isConfirmed === 'Child ' ? '#778692' : 'blue',
+        }}
       >
+        {' '}
         Child
       </div>
     </div>

31bac6af00cc22ebf6c41447cc33a8541345f222
Lukasz Nogaj <lukasznogaj@Lukaszs-Mac-mini.local>
2023-04-02 13:48:57 +0100

    missedclass

--- src/components/Age/TabsAge.jsx
+++ src/components/Age/TabsAge.jsx
@@ -2,7 +2,7 @@ import TabsAgeBtn from './TabsAgeBtn';
 import styles from './TabsAge.module.css';
 const TabsAge = () => {
   return (
-    <div className={styles.TabsAge}>
+    <div className={styles.TabsAges}>
       <TabsAgeBtn />
     </div>
   );
